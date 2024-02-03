c8bf0275c068c45752bad56b2e2835caa79e8865
Javier Rodriguez <xjavierx1995@gmail.com>
2024-02-03 15:09:27 -0400

    show favorites

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,9 @@
 <template>
   <q-page>
-    <TabsComponent :available-tabs="availableTabs"/>
+    <h2>Mostrar favoritos:
+      <q-btn @click="showFavorites" round :style="'background: #F2F2F2; color:' + getColor(favorites)" icon="star" />
+    </h2>
+    <TabsComponent :available-tabs="availableTabs" />
   </q-page>
 </template>
 
@@ -8,6 +11,9 @@
 import { ITabs } from '../interface/Tabs.interface'
 import TabsComponent from 'components/TabsComponent.vue';
 import { ref } from 'vue';
+import { characterStore } from '../stores/characters-store';
+
+const { getFavorites, getCharacters } = characterStore();
 const availableTabs = ref<ITabs[]>([
   {
     label: 'All',
@@ -31,4 +37,15 @@ const availableTabs = ref<ITabs[]>([
   },
 
 ]);
+
+const favorites = ref<boolean>(false)
+
+function showFavorites() {
+  favorites.value = !favorites.value;
+  favorites.value ? getFavorites() : getCharacters();
+}
+
+function getColor(isFav: boolean): string {
+  return isFav ? '#F2994A' : '#828282';
+}
 </script>
